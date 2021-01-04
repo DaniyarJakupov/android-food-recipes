dd69f1640c1ee3ce4c309d241c15992696e30083
Daniyar Jakupov <daniyar.jakupov@wee.com>
2021-01-04 15:49:31 +0100

    Enable bottom tab navigation

--- app/src/main/java/com/daniyarjakupov/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/daniyarjakupov/foodrecipes/MainActivity.kt
@@ -2,10 +2,35 @@ package com.daniyarjakupov.foodrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.daniyarjakupov.foodrecipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }

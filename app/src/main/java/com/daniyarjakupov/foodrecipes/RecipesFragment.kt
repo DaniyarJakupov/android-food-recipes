7f65a1b74dd27aa3157b92f0902b24fffbe738a9
Daniyar Jakupov <daniyar.jakupov@wee.com>
2021-01-06 00:11:37 +0100

    Display Placeholder Row on Recipes Fragment

--- app/src/main/java/com/daniyarjakupov/foodrecipes/RecipesFragment.kt
+++ app/src/main/java/com/daniyarjakupov/foodrecipes/RecipesFragment.kt
@@ -5,18 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.daniyarjakupov.foodrecipes.databinding.FragmentRecipesBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RecipesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecipesFragment : Fragment() {
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        binding.shimmerRecyclerView.showShimmer()
+        return binding.root
     }
 }

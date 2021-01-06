1812ecf313a877da737758776b5623ab6e9514e3
Daniyar Jakupov <daniyar.jakupov@wee.com>
2021-01-06 01:00:31 +0100

    Add retrofit GET method to fetch recipes

--- app/src/main/java/com/daniyarjakupov/foodrecipes/Constants.kt
+++ app/src/main/java/com/daniyarjakupov/foodrecipes/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
 
     companion object {
         const val API_KEY = "6fc3158cde064686866e3425f3b36301"
-        const val BASE_URL = "https://api.spoonacular.com/recipes/complexSearch"
+        const val BASE_URL = "https://api.spoonacular.com"
     }
 }

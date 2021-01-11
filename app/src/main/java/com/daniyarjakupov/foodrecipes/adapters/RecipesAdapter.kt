c640d7767f77f3623daadc273039fe300b8e6452
Daniyar Jakupov <daniyar.jakupov@wee.com>
2021-01-11 11:56:00 +0100

    Create RecipeRowBinding

--- app/src/main/java/com/daniyarjakupov/foodrecipes/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/daniyarjakupov/foodrecipes/adapters/RecipesAdapter.kt
@@ -11,12 +11,12 @@ import com.daniyarjakupov.foodrecipes.utils.RecipesDiffUtil
 
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
 
-    private var recipe = emptyList<Result>()
+    private var recipes = emptyList<Result>()
 
     class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(result: Result) {
-            binding.result = result
+            binding.recipe = result
             binding.executePendingBindings()
         }
 
@@ -34,18 +34,18 @@ class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentResult = recipe[position]
-        holder.bind(currentResult)
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
     }
 
     override fun getItemCount(): Int {
-        return recipe.size
+        return recipes.size
     }
 
     fun setData(newData: FoodRecipe) {
-        val recipesDiffUtil = RecipesDiffUtil(recipe, newData.results)
+        val recipesDiffUtil = RecipesDiffUtil(recipes, newData.results)
         val diffUtilsResult = DiffUtil.calculateDiff(recipesDiffUtil)
-        recipe = newData.results
+        recipes = newData.results
         diffUtilsResult.dispatchUpdatesTo(this)
     }
 }
---
+++ app/src/main/java/com/daniyarjakupov/foodrecipes/bindingadapters/RecipesRowBinding.kt
@@ -0,0 +1,41 @@
+package com.daniyarjakupov.foodrecipes.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.daniyarjakupov.foodrecipes.R
+
+class RecipesRowBinding {
+
+    companion object {
+
+        @BindingAdapter("setNumberOfLikes")
+        @JvmStatic
+        fun setNumberOfLikes(textView: TextView, likes: Int) {
+            textView.text = likes.toString()
+        }
+
+        @BindingAdapter("setCookingTime")
+        @JvmStatic
+        fun setCookingTime(textView: TextView, minutes: Int) {
+            textView.text = minutes.toString()
+        }
+
+        @BindingAdapter("applyVeganColor")
+        @JvmStatic
+        fun applyVeganColor(view: View, vegan: Boolean) {
+            if (vegan) {
+                when (view) {
+                    is TextView -> {
+                        view.setTextColor(ContextCompat.getColor(view.context, R.color.green))
+                    }
+                    is ImageView -> {
+                        view.setColorFilter(ContextCompat.getColor(view.context, R.color.green))
+                    }
+                }
+            }
+        }
+    }
+}

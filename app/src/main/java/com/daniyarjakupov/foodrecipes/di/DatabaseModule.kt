c281790d81f628de5d51d0dcc5522579a8be8278
Daniyar Jakupov <daniyar.jakupov@wee.com>
2021-01-12 21:16:11 +0100

    Create DatabaseModule for dependency injection

---
+++ app/src/main/java/com/daniyarjakupov/foodrecipes/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.daniyarjakupov.foodrecipes.di
+
+import android.content.Context
+import androidx.room.Room
+import com.daniyarjakupov.foodrecipes.data.database.RecipesDatabase
+import com.daniyarjakupov.foodrecipes.utils.Constants.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: RecipesDatabase) = database.recipesDao()
+}

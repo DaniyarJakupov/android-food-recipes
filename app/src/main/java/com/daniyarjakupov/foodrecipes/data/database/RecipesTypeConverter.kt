e966fa87f57ca52e5b3f5d1586b321abe8811555
Daniyar Jakupov <daniyar.jakupov@wee.com>
2021-01-12 11:35:04 +0100

    Create RecipesTypeConverter

---
+++ app/src/main/java/com/daniyarjakupov/foodrecipes/data/database/RecipesTypeConverter.kt
@@ -0,0 +1,21 @@
+package com.daniyarjakupov.foodrecipes.data.database
+
+import androidx.room.TypeConverter
+import com.daniyarjakupov.foodrecipes.models.FoodRecipe
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class RecipesTypeConverter {
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+}
